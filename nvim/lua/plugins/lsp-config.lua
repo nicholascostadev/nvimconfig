dc38e24f015a736b554a267a0ef60eb84128481b
Nicholas Costa <nicholascostadev@gmail.com>
2024-02-07 02:23:19 -0300

    feat: add completions + formatting on save

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,32 +1,46 @@
 return {
   {
     "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "svelte" }
+        ensure_installed = { "lua_ls", "tsserver", "svelte" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.svelte.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+        on_attach = function(client)
+          client.resolved_capabilities.document_formatting = false
+        end
+      })
+      lspconfig.svelte.setup({
+        capabilities = capabilities,
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -3,17 +3,34 @@ return {
   config = function()
     local null_ls = require("null-ls")
 
+    -- Auto format on save
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.eslint_d.with({
-          extra_filetypes = { "svelte" }
+          extra_filetypes = { "svelte" },
         }),
         null_ls.builtins.formatting.prettierd.with({
-          extra_filetypes = { "svelte" }
+          extra_filetypes = { "svelte" },
         }),
-        null_ls.builtins.formatting.prismaFmt
+        null_ls.builtins.formatting.prismaFmt,
       },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
     })
 
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
