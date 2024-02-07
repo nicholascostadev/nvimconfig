4051c389e6ac5edb12c03ddf081f0d027eb91173
Nicholas Costa <nicholascostadev@gmail.com>
2024-02-07 18:09:49 -0300

    refactor: use another cmp for emmet

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,62 +1,49 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		"rafamadriz/friendly-snippets",
-		"mattn/emmet-vim",
-		"dcampos/cmp-emmet-vim",
+	{
+		"jackieaskins/cmp-emmet",
+		build = "npm run release",
 	},
-	config = function()
-		local cmp = require("cmp")
-		require("luasnip.loaders.from_vscode").lazy_load()
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<Escape>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{
-					name = "emmet_vim",
-					option = {
-						filetypes = {
-							"html",
-							"xml",
-							"typescriptreact",
-							"javascriptreact",
-							"css",
-							"sass",
-							"scss",
-							"less",
-							"heex",
-							"tsx",
-							"jsx",
-							"svelte",
-						},
-					},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
 				},
-			}, {
-				{ name = "buffer" },
-			}),
-		})
-	end,
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+					["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<Escape>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{
+						name = "emmet",
+					},
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
---
+++ nvim/lua/plugins/nvim-comment.lua
@@ -0,0 +1,8 @@
+return {
+	"terrortylor/nvim-comment",
+	config = function()
+		local nvim_comment = require("nvim_comment")
+
+		nvim_comment.setup()
+	end,
+}
