1ac03339120b0ae29a17f773d972037c7c7b1cf8
Nicholas Costa <nicholascostadev@gmail.com>
2024-02-16 20:23:38 -0300

    chore: update colorscheme options

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,10 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    local catppuccin = require("catppuccin")
-    catppuccin.setup({
-      transparent_background = true,
-    })
-    vim.cmd.colorscheme("catppuccin")
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		local catppuccin = require("catppuccin")
+		catppuccin.setup({})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
