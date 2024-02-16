689768b5a41a20d5c51c67fa46fb6cb4d6439ca0
Nicholas Costa <nicholascostadev@gmail.com>
2024-02-16 20:24:04 -0300

    feat: use block cursor for insert mode also

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.guicursor = ""
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
 vim.diagnostic.config({
